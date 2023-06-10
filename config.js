df3e5d5c88c9857323e9fec1d42ce49617a40a52
harshshukla2002 <harshshukla2312@gmail.com>
2023-06-10 18:22:05 +0530

    Notes Backend

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  mongoURL:
+    "mongodb+srv://harshshukla:harsh12@cluster0.ob6lhlw.mongodb.net/notes?retryWrites=true&w=majority",
+  port: 8000,
+  secret: "notes",
+};
+
+module.exports = { config };
